0f1df1d75c99757841a478e3386a77e86241fc19
윾승도 <66948129+tmdeh@users.noreply.github.com>
2024-02-16 23:33:48 +0900

    init

---
+++ src/main/java/com/example/pharmacyrecommdation/PharmacyRecommdationApplication.java
@@ -0,0 +1,13 @@
+package com.example.pharmacyrecommdation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyRecommdationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PharmacyRecommdationApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/pharmacyrecommdation/PharmacyRecommdationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pharmacyrecommdation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyRecommdationApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
